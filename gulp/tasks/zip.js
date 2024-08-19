db8e29f2b7ad2a3e02a662e23d63ada0850cb516
PsychoPuss <evgeniy.v.ilyin@gmail.com>
2024-08-19 04:06:51 +0300

    gulp zip task encoding: false

--- gulp/tasks/zip.js
+++ gulp/tasks/zip.js
@@ -3,7 +3,7 @@ import zipPlugin from "gulp-zip";
 export const zip = () => {
 	app.plugins.deleteAsync(`./${app.path.rootFolder}.zip`);
 	return app.gulp
-		.src(`${app.path.buildFolder}/**/*.*`, {})
+		.src(`${app.path.buildFolder}/**/*.*`, { encoding: false })
 		.pipe(
 			app.plugins.plumber(
 				app.plugins.notify.onError({
